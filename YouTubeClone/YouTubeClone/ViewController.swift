f6946dda8c38dd58987a303c3894cd7b84683d8e
Mario Antonio Baca Timagen <marbacvaca@gmail.com>
2022-09-21 16:52:33 -0500

    #CLASE 2

---
+++ YouTubeClone/YouTubeClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YouTubeClone
+//
+//  Created by CSA MEX on 20/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
